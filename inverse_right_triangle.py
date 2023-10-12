d7b082a94f87ccf3cfb5022e9dd879a25d59d846
gcfsalvador <145944423+gcfsalvador@users.noreply.github.com>
2023-10-12 19:53:25 +0800

    Add files via upload

---
+++ inverse_right_triangle.py
@@ -0,0 +1,12 @@
+##Gian Cyrus F. Salvador
+#CITCS-1B-CC2
+#inverse_right_triangle
+
+rows = 5
+for i in range(rows, 1, -1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print("")
+print("1")
+
+print("=========")
