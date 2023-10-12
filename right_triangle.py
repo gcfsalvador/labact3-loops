6a4e51cd4d44dc2661e25d6ca4da97e3cd2b8818
gcfsalvador <145944423+gcfsalvador@users.noreply.github.com>
2023-10-12 19:51:50 +0800

    Add files via upload

---
+++ right_triangle.py
@@ -0,0 +1,12 @@
+##Gian Cyrus F. Salvador
+#CITCS-1B-CC2
+
+#number pattern using a loop
+
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print("")
+
+print("=========")
---
+++ summation.py
@@ -0,0 +1,11 @@
+##Gian Cyrus F. Salvador
+#CITCS-1B-CC2
+#following number pattern using a loop
+
+size = 10
+sum=0
+for i in range(1, size + 1):
+    sum +=i
+print(sum)
+
+print("=========")
